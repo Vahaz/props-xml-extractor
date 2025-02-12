6e0a47a3d72fd49a1003fa82497f2ba6bd391025
Vahaz <netherdream30@gmail.com>
2025-02-12 20:06:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[Archived] A small project for a friend. 
+
+It extract all entities/props and items used in xml (.YTYP.XML) files from CodeWalker / GTA V. (Mostly used for FiveM)
+The project will be ARCHIVED on release.
+
+## USAGE
+
+1. Get a '[name].ytyp.xml' file from CodeWalker, rename it 'data.xml'.
+2. Run the main.js file. (node .)
+3. Read the created 'result.txt' file containing all not-duplicated props / entities used in the original ytyp file.
+
+## TOOLS
+
+- Visual Studio Code
+- Node.JS
+- @xml2js
+- @xmlbuilder
---
+++ main.js
@@ -0,0 +1,42 @@
+var fs = require('fs'),
+    xml2js = require('xml2js');
+    util = require('util');
+
+var parser = new xml2js.Parser();
+
+var storage = new Map(); 
+var output = new Map();
+
+fs.readFile('data.xml', "utf-8", function(err, data) {
+    parser.parseString(data, function (err, result) {
+
+        const Items = result.CMapTypes.archetypes[0].Item;
+        var index = 0;
+        Items.forEach((Item) => {
+            // ITEMS
+            storage.set((index += 1), Item.name[0]);
+
+            // ITEMS → EXTENSIONS → ITEMS
+            const Props = Item.extensions[0].Item;
+            if (Props != undefined) {
+                Props.forEach((prop) => { storage.set((index += 1), prop.spawnType[0]) });
+            };
+
+            // ITEMS → ENTITIES → ITEMS
+            if (Item.entities != undefined) {
+                const Entities = Item.entities[0].Item;
+                Entities.forEach((entity) => { storage.set((index += 1), entity.archetypeName[0]) })
+            };
+        });
+
+        storage.forEach((item) => {
+            if (!output.has(item)) {
+                output.set(item, undefined);
+            };
+        });
+
+        var json = JSON.stringify([...output.keys()], null, 1).replaceAll('[', '').replaceAll('"', '').replaceAll(',', '').replaceAll(']', '').replaceAll(' ', ''); // How regex works???
+        fs.writeFile('result.txt', json, err => {});
+        console.log(`Done, got ${output.size} props !`);
+    });
+});

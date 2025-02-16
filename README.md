8b31ea363028c0c179db4938a02fac1d14c607e9
Vahaz <netherdream30@gmail.com>
2025-02-16 15:21:04 +0100

    1.2.0 update

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 [Archived] A small project for a friend. 
 
-It extract all entities/props and items used in xml (.YTYP.XML) files from CodeWalker / GTA V. (Mostly used for FiveM)
+This tool extract all props and items used in a .xml file from CodeWalker / GTA V. (Mostly used for FiveM)
+It is used to know which props are used in a mapping (to import them later) so you do not have to import every props in blender (or other software).
+
 The project will be ARCHIVED on release.
 
 ## USAGE
 
-1. Get a '[name].ytyp.xml' file from CodeWalker, rename it 'data.xml' and add it to the folder.
-2. Run the main.js file. (node .)
-3. Read the created 'result.txt' file containing all not-duplicated props / entities used in the original ytyp file.
+1. In CodeWalker, extract a .ytyp file in .xml format, should get something like '[file_name].ytyp.xml'. 
+2. Rename the .xml file to 'data.xml' and add it to the folder.
+3. Run the main.js file using Node.JS (node .)
+4. Read the created 'result.txt' file in the folder, containing all not-duplicated props used in the .ytyp file.
 
 ## TOOLS
 
@@ -15,3 +18,19 @@ The project will be ARCHIVED on release.
 - Node.JS
 - @xml2js
 - @xmlbuilder
+
+## UPDATES
+
+- 1.2.0
+- - added props from entity sets.
+- - update readme.
+- - update package version / description.
+- - update console log messages.
+
+- 1.1.0
+- - syntax update in the code.
+- - update readme.
+- - remove unused "require".
+
+- 1.0.0
+- - initial release.
--- main.js
+++ main.js
@@ -2,33 +2,47 @@ var fs = require('fs'),
     xml2js = require('xml2js');
 
 var parser = new xml2js.Parser();
-
 var storage = new Map(); 
 var output = new Map();
 
 fs.readFile('data.xml', "utf-8", function(err, data) {
     parser.parseString(data, function (err, result) {
+        if(result == undefined) {
+            console.warn("[prop-xml-extractor] data.xml is empty / do not exist.")
+            return;
+        }
 
         const Items = result.CMapTypes.archetypes[0].Item;
         var index = 0;
         Items.forEach((Item) => {
-            // ITEMS
+            // Props
             if(Item.name[0] != undefined) storage.set((index += 1), Item.name[0]);
 
-            // ITEMS → EXTENSIONS → ITEMS
+            // Extensions Props
             const Props = Item.extensions[0].Item;
-            if (Props != undefined) {
+            if(Props != undefined) {
                 Props.forEach((prop) => { 
                     if(prop.spawnType != undefined) storage.set((index += 1), prop.spawnType[0]);
                 });
             };
 
-            // ITEMS → ENTITIES → ITEMS
-            if (Item.entities != undefined) {
+            // Entities Props
+            if(Item.entities != undefined) {
                 const Entities = Item.entities[0].Item;
                 Entities.forEach((entity) => { 
                     if(entity.archetypeName != undefined) storage.set((index += 1), entity.archetypeName[0]);
                 })
+            };
+
+            // Entities Sets Props
+            if (Item.entitySets != undefined) {
+                const EntitySetsItems = Item.entitySets[0].Item;
+                EntitySetsItems.forEach((item) => {
+                    const Entities = item.entities[0].Item;
+                    Entities.forEach((object) => {
+                        if(object.archetypeName != undefined) storage.set((index += 1), object.archetypeName[0]);
+                    });
+                });
             };
         });
 
@@ -40,6 +54,6 @@ fs.readFile('data.xml', "utf-8", function(err, data) {
 
         var json = JSON.stringify([...output.keys()], null, 1).replaceAll('[', '').replaceAll('"', '').replaceAll(',', '').replaceAll(']', '').replaceAll(' ', ''); // How regex works???
         fs.writeFile('result.txt', json, err => {});
-        console.log(`Done, got ${output.size} props !`);
+        console.log(`[prop-xml-extractor] Done, got ${output.size} props !`);
     });
 });
